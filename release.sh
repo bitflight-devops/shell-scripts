2e96582732f056c164d263afcbe82268058783df
Jamie Nelson <jamie@wearsafe.com>
2022-11-08 16:19:04 -0500

    chore(release): fix release tagging script

--- release.sh
+++ release.sh
@@ -12,5 +12,6 @@ git tag -a "${stub_major_minor}" -m "Release ${newtag}"
 git tag -a "${newtag}" -m "Release ${newtag}"
 git push origin ":${stub_major}" 2>/dev/null || true
 git push origin ":${stub_major_minor}" 2>/dev/null || true
+git push origin ":${newtag}" 2>/dev/null || true
 git push --tags
 git push
